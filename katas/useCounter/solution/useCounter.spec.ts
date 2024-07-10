fc2b75a2678e09f3cd4a8a848f695f9b6e0546f0
Verthon <christopher.sordyl@gmail.com>
2024-07-10 17:33:38 +0200

    feat: add complete useCounter kata

---
+++ katas/useCounter/solution/useCounter.spec.ts
@@ -0,0 +1,82 @@
+import { act, renderHook } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { useCounter } from "./useCounter";
+
+describe("useCounter", () => {
+	it("should accept optional initialCount parameter", () => {
+		const { result } = renderHook(() => useCounter({ initialCount: 1 }));
+
+		expect(result.current.count).toBe(1);
+	});
+
+	it("should accept optional min and max parameters", () => {
+		const { result } = renderHook(() => useCounter({ min: 0, max: 5 }));
+
+		expect(result.current.count).toBe(0);
+	});
+
+	it("should increment current value", async () => {
+		const { result } = renderHook(() => useCounter({}));
+
+		expect(result.current.count).toBe(0);
+
+		act(() => {
+			result.current.increment();
+		});
+
+		expect(result.current.count).toBe(1);
+	});
+
+	it("should decrement current value", async () => {
+		const { result } = renderHook(() => useCounter({ initialCount: 5 }));
+
+		expect(result.current.count).toBe(5);
+
+		act(() => {
+			result.current.decrement();
+		});
+
+		expect(result.current.count).toBe(4);
+	});
+
+	it("should reset current value to the initial one", async () => {
+		const { result } = renderHook(() => useCounter({}));
+
+		expect(result.current.count).toBe(0);
+
+		act(() => {
+			result.current.decrement();
+			result.current.increment();
+			result.current.increment();
+			result.current.reset();
+		});
+
+		expect(result.current.count).toBe(0);
+	});
+
+	it("should not increase the count above max value", () => {
+		const { result } = renderHook(() =>
+			useCounter({ initialCount: 3, max: 4 })
+		);
+
+		act(() => {
+			result.current.increment();
+			result.current.increment();
+		});
+
+		expect(result.current.count).toBe(4);
+	});
+
+	it("should not decrease the count above min value", () => {
+		const { result } = renderHook(() =>
+			useCounter({ initialCount: -9, min: -10 })
+		);
+
+		act(() => {
+			result.current.decrement();
+			result.current.decrement();
+		});
+
+		expect(result.current.count).toBe(-10);
+	});
+});
---
+++ katas/useCounter/solution/useCounter.ts
@@ -0,0 +1,55 @@
+import { useReducer, useState } from "react";
+
+type UseCounterParams = {
+	initialCount?: number;
+	min?: number;
+	max?: number;
+};
+
+type Action =
+	| { type: "incremented"; payload: { max: number } }
+	| { type: "decremented"; payload: { min: number } }
+	| { type: "reseted"; payload: { initialCount: number } };
+
+const reducer = (count: number, action: Action) => {
+	if (action.type === "incremented") {
+		return Math.min(count + 1, action.payload.max);
+	}
+
+	if (action.type === "decremented") {
+		return Math.max(count - 1, action.payload.min);
+	}
+
+	if (action.type === "reseted") {
+		return action.payload.initialCount;
+	}
+
+	return count;
+};
+
+export const useCounter = ({
+	initialCount = 0,
+	min = -Infinity,
+	max = Infinity,
+}: UseCounterParams) => {
+	const [count, dispatch] = useReducer(reducer, initialCount);
+
+	const increment = () => {
+		dispatch({ type: "incremented", payload: { max } });
+	};
+
+	const decrement = () => {
+		dispatch({ type: "decremented", payload: { min } });
+	};
+
+	const reset = () => {
+		dispatch({ type: "reseted", payload: { initialCount } });
+	};
+
+	return {
+		increment,
+		decrement,
+		reset,
+		count,
+	};
+};
